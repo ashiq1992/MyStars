1b79be20ef6b9ffd74972404a61c2a4bcd64eb04
Ameen <Ameen@DESKTOP-OOVLOTC>
2017-04-03 00:09:14 +0800

    made changes to add module by student.Result: success

--- MyStars/src/Control/AddDropController.java
+++ MyStars/src/Control/AddDropController.java
@@ -16,126 +16,138 @@ import Control.FileManager;
 import Model.Student;
 import Model.AddDrop;
 
-
 public class AddDropController {
 	public static final String SEPARATOR = "|";
 	public static final String INDEX_SEPARATOR = "_";
-	
+
 	private Course c1;
 	private Admin a1;
 	private Student s1;
 	private FileManager f1;
-	private CourseController Cc1= new CourseController();
-	
-	public boolean addMethod(String courseCode,int index,String matricNum)
-	{
-		boolean endResult=false;
+	private CourseController Cc1 = new CourseController();
+
+	public boolean addMethod(String courseCode, int index, String matricNum) {
+		boolean endResult = false;
 		List student;
 		AddDrop Add;
-		//Cc1.decreaseCourseIndexVacancy(courseCode, index);
-try {
-			//ArrayList<AddDrop> a1 =Cc1. readAll("src/courseAndStudent.txt");//To add the student with a course then save it to a txt file
-			List studentData = new ArrayList() ;
+		// Cc1.decreaseCourseIndexVacancy(courseCode, index);
+		try {
+			// ArrayList<AddDrop> a1 =Cc1.
+			// readAll("src/courseAndStudent.txt");//To add the student with a
+			// course then save it to a txt file
+			List studentData = new ArrayList();
+
+			ArrayList a1 = Cc1.readAllCourse("src/courses.txt");
+			//Comment: Reads all the data from courses.txt file and stores it into an ArrayList
 			
-			ArrayList<Course> a1 =Cc1. readAllCourse("src/courses.txt");//read all data and store it in course format
-			List data = new ArrayList() ;
-			
+			List data = new ArrayList();
+			courseCode= courseCode.toLowerCase();
 			for (int x = 0; x < a1.size(); x++) {
-				if (courseCode.equals(a1.get(x).getCourseCode())) {
-					if (a1.get(x).getVacancy()[index-1] <= 0) {
-						//waitList();
+					Course course = (Course)a1.get(x);
+					
+				if (course.getCourseCode().toLowerCase().equals(courseCode)) {
+					
+					if (course.getVacancy()[index - 1] <= 0) {
+						// waitList();
 						endResult = false;
 					} else {
-						a1.get(x).decreaseVacancy(index);
-						Add=new AddDrop(courseCode,index,matricNum);
+						
+						course.decreaseVacancy(index);
+						Add = new AddDrop(courseCode, index, matricNum);
 						studentData.add(Add);
-						//System.out.println("Index:"+index+" "+a1.get(x).getVacancy()[index-1]);
-					
+						// System.out.println("Index:"+index+"
+						// "+a1.get(x).getVacancy()[index-1]);
+
 						endResult = true;
 					}
-					
+
 				}
 				data.add(a1.get(x));
 			}
 			
-			
-						
-			AddDropController.saveCourseWithStudent("src/courseAndStudent.txt", studentData,courseCode,index);
+//			for(int i=0;i<studentData.size();i++)
+//			{
+//				AddDrop course = (AddDrop)studentData.get(i);
+//						
+//						System.out.println("Test: "+course.getCourseCode());
+//			}
+
+			AddDropController.saveCourseWithStudent("src/courseAndStudent.txt",studentData,courseCode,index);
 			Cc1.saveCourseAmend("src/courses.txt",data);
-			endResult=true;
+			endResult = true;
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
-		
+
 		return endResult;
-		
+
 	}
-	
-	public void sendEmail()
-	{
-	
+
+	public void sendEmail() {
+
 	}
-	
-	public void dropMethod()
-	{
-	
+
+	public void dropMethod() {
+
 	}
-	
-	public void changeIndex()
-	{
-	
+
+	public void changeIndex() {
+
 	}
-	
-	public void swapIndex()
-	{
-	
+
+	public void swapIndex() {
+
 	}
-	
-	public void checkClash()
-	{
-	
+
+	public void checkClash() {
+
 	}
-	
-	
-	public static void saveCourseWithStudent(String filename, List list,String courseCode,int index) throws IOException {
+
+	public static void saveCourseWithStudent(String filename, List list, String courseCode, int index)
+			throws IOException {
 		List tempList = new ArrayList();// to store students data
-		ArrayList<AddDrop> StringArray=readAllCourseAndStudent(filename);
+		ArrayList StringArray = readAllCourseAndStudent("src/courses.txt");
 		
-		for(int x=0;x<StringArray.size();x++){
-			AddDrop Add = (AddDrop) list.get(0);//Convert the data into AddDrop class then check for condition before adding them together
-			if(StringArray.get(x).getCourseCode().equals(courseCode) && StringArray.get(x).getIndex()==index){
-				StringArray.get(x).setmatricNumArray(Add.getMatricNum());
+		
+		for (int x = 0; x < StringArray.size(); x++) {
+			AddDrop Add = (AddDrop) list.get(0);// Convert the data into AddDrop
+												// class then check for
+												// condition before adding them
+												// together
+			
+			AddDrop stArray = (AddDrop) StringArray.get(x);
+			//System.out.println("test");
+			if ((stArray.getCourseCode().toLowerCase().equals(courseCode.toLowerCase())) && (stArray.getIndex() == index)) {
+				System.out.println(Add.getMatricNum());
+				stArray.setmatricNumArray(Add.getMatricNum());
+				
 			}
 		}
-		
-		
-		
-		
-		for (int i = 0; i <StringArray.size(); i++) {
-			//AddDrop Add = (AddDrop) list.get(i);
+
+		for (int i = 0; i < list.size(); i++) {
+			// AddDrop Add = (AddDrop) list.get(i);
 			StringBuilder st = new StringBuilder();
-		
-			st.append(StringArray.get(i).getCourseCode());
-			st.append(INDEX_SEPARATOR);
-			st.append(StringArray.get(i).getIndex());
-			for(int j=0;j<StringArray.get(i).getMatricNumArray().length;j++){
-				st.append(StringArray.get(i).getMatricNumArray()[j]);
+			AddDrop stArray = (AddDrop) list.get(i);
+			st.append(stArray.getCourseCode());
+			st.append(SEPARATOR);
+			st.append(stArray.getIndex());
+			//System.out.println(stArray.getMatricNum());
+			//for (int j = 0; j < stArray.getMatricNumArray().length; j++) {
+			st.append(SEPARATOR);	
+			st.append(stArray.getMatricNum());
 				st.append(SEPARATOR);
-			}
-			
-			
-			
+			//}
+
 			// String temp = "" + course.getCapacity();
 			// st.append(temp.trim());
 			// st.append(SEPARATOR);
 
 			tempList.add(st.toString());
 		}
+		
+		
 		write(filename, tempList);
 	}
 
@@ -158,12 +170,11 @@ try {
 			out.close();
 		}
 	}
-	
-	
-	
+
 	public static ArrayList readAllCourseAndStudent(String filename) throws IOException {
 		// read String from text file
 		ArrayList stringArray = (ArrayList) read(filename);
+		System.out.println(read(filename).size());
 		ArrayList alr = new ArrayList();// to store Admins data
 
 		for (int i = 0; i < stringArray.size(); i++) {
@@ -181,16 +192,14 @@ try {
 																		// using
 																		// delimiter
 																		// ","
-			
-		
+
 			String courseCode = star.nextToken("_").trim();
 			String index = star.nextToken("|").trim();
 			StringBuilder modifyIndex = new StringBuilder(index);
 			index = modifyIndex.deleteCharAt(0).toString();
-			//System.out.println(index);//for error checking
+			// System.out.println(index);//for error checking
 			int counter = 0;
 			counter = star.countTokens();
-		
 
 			String[] retrievedMatricNum = new String[counter];
 
@@ -198,18 +207,22 @@ try {
 			for (int j = 0; j < counter; j++) {
 
 				String matricNum = star.nextToken().trim();
-				//System.out.println(matricNum);//for testing purpose
-				retrievedMatricNum[j] =matricNum;
+				// System.out.println(matricNum);//for testing purpose
+				retrievedMatricNum[j] = matricNum;
 
 			}
 
-			AddDrop add = new AddDrop(courseCode,Integer.parseInt(index), retrievedMatricNum);
+			AddDrop add = new AddDrop(courseCode, Integer.parseInt(index), retrievedMatricNum);
 			// Course course = new
 			// Course(courseCode,courseName,school,capacity,startDate,endDate);
 
 			// add student objects to alr
+			//System.out.println("hello");
 			alr.add(add);
 		}
+		
+		
+		
 		return alr;
 	}
 
@@ -226,30 +239,32 @@ try {
 		}
 		return data;
 	}
+
+//	 public static void main(String[] args){
+//	 try {
+//	 ArrayList A1= readAllCourseAndStudent("src/courseAndStudent.txt");
+//	 System.out.println(A1.size());
+//	 for(int x=0;x<A1.size();x++){
+//		 AddDrop a1 = (AddDrop) A1.get(x);
+//	 for(int i=0;i<a1.getMatricNumArray().length;i++){
+//	 System.out.println(a1.getCourseCode()+" "+a1.getIndex() +" "+a1.getMatricNumArray()[i]);
+//	 }
+//	
+//	 //System.out.println(A1.get(x).getIndex() );
+//	
+//	 }
+//	 } catch (IOException e) {
+//	 // TODO Auto-generated catch block
+//	 e.printStackTrace();
+//	 }
+//	
+//	 }
 	
-	
-	
-//	public static void main(String[] args){
-//		 try {
-//			ArrayList<AddDrop> A1= readAllCourseAndStudent("src/courseAndStudent.txt");
-//			for(int x=0;x<A1.size();x++){
-//				for(int i=0;i<A1.get(x).getMatricNumArray().length;i++){
-//					System.out.println(A1.get(x).getCourseCode()+" "+A1.get(x).getIndex() +" "+A1.get(x).getMatricNumArray()[i]);
-//				}
-//				
-//				//System.out.println(A1.get(x).getIndex() );
-//				
-//			}
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		 
+//	public static void main(String args[])
+//	
+//	{
+//		AddDropController a1 = new AddDropController();
+//		a1.addMethod("Mh1815",1,"u162");
 //	}
-	
-	
-	
-	
-	
 
 }
--- MyStars/src/View/StarsPlannerApp.java
+++ MyStars/src/View/StarsPlannerApp.java
@@ -11,96 +11,85 @@ import Control.StudentController;
 import Model.Student;
 
 public class StarsPlannerApp {
-	static AdminController   Ad = new AdminController();
-	static CourseController  Cd = new CourseController();
+	static AdminController Ad = new AdminController();
+	static CourseController Cd = new CourseController();
 	static StudentController Sd = new StudentController();
-	static AddDropController addDrop=new AddDropController();
-	static Student student =new Student();
-	
+	static AddDropController addDrop = new AddDropController();
+	static Student student = new Student();
+
 	static boolean adddropEnable;
 	static Scanner sc = new Scanner(System.in);
-	
-	public static void main(String args[]){
+
+	public static void main(String args[]) {
 		adddropEnable = true;
-		
-		int input=-1; //Takes in user input;
-		int valid=1;
+
+		int input = -1; // Takes in user input;
+		int valid = 1;
 		boolean result;
-		String password,userId;
+		String password, userId;
 		do {
-		 
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Type of user: ");
-		System.out.println("1) Student ");
-		System.out.println("2) Admin ");
-		
-		
-		try
-		{
-			input=sc.nextInt();
-		}catch(InputMismatchException ex)
-		{
-			System.out.println("Input choice is not an integer");
-		}
-		if(input==1)
-		{
-			System.out.println("Enter userId:");
-			userId=sc.next();
-			System.out.println("Enter Password:");
-			password=sc.next();
-			
-			result=Sd.checkAccount(userId, password);
-			if(result==true){
-				System.out.println("Sucess");
-				student.setMatriculationNumber(userId);
-				StudentMenu();
-				
+
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Type of user: ");
+			System.out.println("1) Student ");
+			System.out.println("2) Admin ");
+
+			try {
+				input = sc.nextInt();
+			} catch (InputMismatchException ex) {
+				System.out.println("Input choice is not an integer");
 			}
-			else{
-				System.out.println("Failed");
+			if (input == 1) {
+				System.out.println("Enter userId:");
+				userId = sc.next();
+				System.out.println("Enter Password:");
+				password = sc.next();
+
+				result = Sd.checkAccount(userId, password);
+				if (result == true) {
+					System.out.println("Sucess");
+					student.setMatriculationNumber(userId);
+					StudentMenu();
+
+				} else {
+					System.out.println("Failed");
+				}
 			}
-		}
-		
-		else if (input==2)
-		{
-			System.out.println("Enter userId:");
-			userId=sc.next();
-			System.out.println("Enter Password:");
-			password=sc.next();
-			AdminController ad=new AdminController();
-			result=ad.checkAccount(userId, password);
-			if(result==true){
-				System.out.println("Sucess");
-				AdminMenu();
-				
+
+			else if (input == 2) {
+				System.out.println("Enter userId:");
+				userId = sc.next();
+				System.out.println("Enter Password:");
+				password = sc.next();
+				AdminController ad = new AdminController();
+				result = ad.checkAccount(userId, password);
+				if (result == true) {
+					System.out.println("Sucess");
+					AdminMenu();
+
+				} else {
+					System.out.println("Failed");
+				}
 			}
-			else{
-				System.out.println("Failed");
+
+			else {
+				System.out.println("Invalid Choice/Input! Re-enter");
+
 			}
 		}
-		
-		else 
-		{
-			System.out.println("Invalid Choice/Input! Re-enter");
-			
-		}
-		}
-		
-		while(valid !=0);
-			
-		
+
+		while (valid != 0);
+
 	}
-	
+
 	// =================================================================================
 	// =================================================================================
-	// 									ADMIN MENU
+	// ADMIN MENU
 	// =================================================================================
 	// =================================================================================
-	
-	public static void AdminMenu()
-	{
-		Scanner sc=new Scanner(System.in);
+
+	public static void AdminMenu() {
+		Scanner sc = new Scanner(System.in);
 		int input;
 		System.out.println("1)Add student.");
 		System.out.println("2)Add Course.");
@@ -110,220 +99,220 @@ public class StarsPlannerApp {
 		System.out.println("6) Remove Student");
 		System.out.println("7) Enable/Disable Add/Drop");
 		System.out.println("10)LogOut.");
-		
-		//System.out.println("Enter options:");
-		//input=sc.nextInt();
-		
-		do{
+
+		// System.out.println("Enter options:");
+		// input=sc.nextInt();
+
+		do {
 			System.out.println("Enter options:");
-			input=sc.nextInt();
-			
-		switch(input){
-		case 1: AddStudent();
+			input = sc.nextInt();
+
+			switch (input) {
+			case 1:
+				AddStudent();
 				break;
 
-		case 2: addCourse();
-				break;
-				
-		case 3: removeCourse();
-				break;
-				
-		case 6: removeStudent();
-				break;
-		
-		case 7: adddrop();
+			case 2:
+				addCourse();
 				break;
 
-			
-			default: System.out.println("Input error");
-		}
-		}while(input !=10);
-		
+			case 3:
+				removeCourse();
+				break;
+
+			case 6:
+				removeStudent();
+				break;
+
+			case 7:
+				adddrop();
+				break;
+
+			default:
+				System.out.println("Input error");
+			}
+		} while (input != 10);
+
 	}
-	
-	public static void AddStudent(){
+
+	public static void AddStudent() {
 		boolean result;
-		
-		
-		result=Ad.AddStudent();
-		if(result==true){
+
+		result = Ad.AddStudent();
+		if (result == true) {
 			System.out.println("Success");
-		}
-		else{
+		} else {
 			System.err.println("Fail");
 		}
 	}
-	
-	public static void addCourse(){
+
+	public static void addCourse() {
 		boolean result;
-		
+
 		result = Cd.addCourse();
-		if(result==true){
+		if (result == true) {
 			System.out.println("Success");
-		}
-		else{
+		} else {
 			System.err.println("Fail");
 		}
 	}
-	
-	public static void removeCourse(){
+
+	public static void removeCourse() {
 		boolean result;
-		Scanner sc=new Scanner(System.in);
-		CourseController cd =new CourseController();
+		Scanner sc = new Scanner(System.in);
+		CourseController cd = new CourseController();
 		System.out.println("Enter CourseCode:");
-		String id=sc.nextLine();
-		result=cd.deleteCourse("src/course.txt",id);
-		if(result==true){
+		String id = sc.nextLine();
+		result = cd.deleteCourse("src/course.txt", id);
+		if (result == true) {
 			System.out.println("Success");
-		}
-		else{
+		} else {
 			System.err.println("Fail");
 		}
 	}
-	
-	
-	public static void removeStudent(){
+
+	public static void removeStudent() {
 		boolean result;
 		StudentController s1 = new StudentController();
 		System.out.println("Enter Student MatricID:");
-		String id=sc.nextLine();
-		result=s1.deleteStudent("src/student.txt",id);
-		if(result==true){
+		String id = sc.nextLine();
+		result = s1.deleteStudent("src/student.txt", id);
+		if (result == true) {
 			System.out.println("Success");
-		}
-		else{
+		} else {
 			System.err.println("Fail");
 		}
 	}
-	
-	public static void adddrop(){
+
+	public static void adddrop() {
 		int input;
 		System.out.println("Current Add/Drop Status, Enabled: " + adddropEnable);
-		
-		if(adddropEnable){
+
+		if (adddropEnable) {
 			System.out.println("Do you want to disable Add/Drop?\n1. Yes\n2. No");
-			input =sc.nextInt();
-			if(input == 1 ){
+			input = sc.nextInt();
+			if (input == 1) {
 				adddropEnable = false;
 			}
 		} else {
 			System.out.println("Do you want to enable Add/Drop?\n1. Yes\n2. No");
-			input =sc.nextInt();
-			if(input == 1 ){
+			input = sc.nextInt();
+			if (input == 1) {
 				adddropEnable = true;
 			}
 		}
-		
+
 	}
 
 	// =================================================================================
 	// =================================================================================
-	// 									STUDENT MENU
+	// STUDENT MENU
 	// =================================================================================
 	// =================================================================================
-	
-	public static void StudentMenu()
-	{
+
+	public static void StudentMenu() {
 		int input;
-		System.out.println("1)View all Courses.");
-		System.out.println("2)View my Courses.");
-		System.out.println("3)Add/Drop.");
-		System.out.println("0)LogOut.");
-		
-		//System.out.println("Enter options:");
-		//input=sc.nextInt();
-		
-		do{
-			System.out.println("Enter options:");
-			input=sc.nextInt();
-			
-			switch(input){
-			case 1: viewAllCourses();
-					break;
-	
-			case 2: viewMyCourses();
-					break;
-						
-			case 3: addDropMenu();
-					break;
-							
-				default: System.err.println("Input error");
+		do {
+			System.out.println("                                         ");
+			System.out.println("*****************************************");
+			System.out.println("Student Menu");
+			System.out.println("1)View all Courses.");
+			System.out.println("2)View my Courses.");
+			System.out.println("3)Add/Drop.");
+			System.out.println("0)LogOut.");
+			System.out.println("*****************************************");
+			System.out.print("Enter options: ");
+			input = sc.nextInt();
+
+			switch (input) {
+			case 1:
+				viewAllCourses();
+				break;
+
+			case 2:
+				viewMyCourses();
+				break;
+
+			case 3:
+				addDropMenu();
+				break;
+
+			default:
+				System.err.println("Input error");
 			}
-			
-		} while(input !=0);
+
+		} while (input != 0);
 	}
-	
-	public static void  viewAllCourses(){
-		
+
+	public static void viewAllCourses() {
+
 		Cd.showAllCourses();
 	}
-	
-	public static void viewMyCourses(){
-		
+
+	public static void viewMyCourses() {
+
 	}
-	
-	
+
 	// =================================================================================
-		// =================================================================================
-		// 									AddDrop Menu
-		// =================================================================================
-		// =================================================================================
-	
-	
-	public static void addDropMenu(){
+	// =================================================================================
+	// AddDrop Menu
+	// =================================================================================
+	// =================================================================================
+
+	public static void addDropMenu() {
 		int input;
 		System.out.println("1)Add a course.");
 		System.out.println("2)Drop a course.");
 		System.out.println("3)Change index number.");
 		System.out.println("0)Back.");
-		
+
 		System.out.println("Enter options:");
-		input=sc.nextInt();
-		
-		switch(input){
-			case 1: addACourse();
-					break;
-	
-			case 2: dropACourse();
-					break;
-						
-			case 3: changeIndex();
-					break;
-					
-			default: System.err.println("Input error");
+		input = sc.nextInt();
+
+		switch (input) {
+		case 1:
+			addACourse();
+			break;
+
+		case 2:
+			dropACourse();
+			break;
+
+		case 3:
+			changeIndex();
+			break;
+
+		default:
+			System.err.println("Input error");
 		}
 	}
-	
-	public static void addACourse(){
+
+	public static void addACourse() {
 		String courseCode;
 		int index;
 		boolean result;
 		System.out.println("Enter Course Code: ");
-		courseCode=sc.next();
-		//add
+		courseCode = sc.next();
+		// add
 		Cd.showIndexByCourse(courseCode);
 		System.out.println("Enter index: ");
-		index=sc.nextInt();
-		result=addDrop.addMethod(courseCode, index,student.getMatriculationNumber());
-		//Cd.decreaseCourseIndexVacancy(courseCode, index);
-		
-		if(result){
+		index = sc.nextInt();
+		result = addDrop.addMethod(courseCode, index, student.getMatriculationNumber());
+		// Cd.decreaseCourseIndexVacancy(courseCode, index);
+
+		if (result) {
 			System.out.println("Success");
-		}
-		else{
+		} else {
 			System.err.println("Fail");
 		}
-		
-		
-		
-		
+
 	}
-	
-	public static void dropACourse(){
-		
+
+	public static void dropACourse() {
+
 	}
-	
-	public static void changeIndex(){
-		
+
+	public static void changeIndex() {
+
 	}
 }
